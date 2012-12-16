d27ab4b0ed704f1fe8b3b853e1ff3e501acf7e98
Victor Christensen <tapn2it@mac.com>
2012-12-15 20:25:15 -0800

    init commit

---
+++ src-clj/example/routes.clj
@@ -0,0 +1,24 @@
+(ns example.routes
+  (:use compojure.core
+        example.views
+        [hiccup.middleware :only (wrap-base-url)])
+  (:require [compojure.route :as route]
+            [compojure.handler :as handler]
+            [compojure.response :as response]
+            [ring.adapter.jetty :as ring])
+  (:gen-class ))
+
+(defroutes main-routes
+  (GET "/" [] (index-page))
+  (GET "/repl-demo" [] (repl-demo-page))
+  (route/resources "/")
+  (route/not-found "Page not found"))
+
+(def app
+  (-> (handler/site main-routes)
+    (wrap-base-url)))
+
+(defn -main [& m]
+  (let [mode (keyword (or (first m) :dev))
+        port (Integer. (get (System/getenv) "PORT" "8080"))]
+    (ring/run-jetty #'main-routes {:port port :join? false})))
